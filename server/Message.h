26a5860f653fd0d1f7abba43af9463da995a5fab
paologastaldi <s277393@studenti.polito.it>
2020-11-21 11:15:07 +0100

    feat: message creation and sending
    manage boost buffers automatically

--- server/Message.h
+++ server/Message.h
@@ -6,7 +6,12 @@
 #define SERVER_MESSAGE_H
 
 #include <iostream>
+#include <sstream>
 #include <vector>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/tuple/tuple.hpp>
+#include <boost/bind.hpp>
 #include <boost/archive/text_oarchive.hpp>
 #include <boost/archive/text_iarchive.hpp>
 
@@ -24,7 +29,8 @@ enum MESSAGE_TYPE : int {
     GET = 4,
     GET_CONTENT = 5,
     PUSH = 6,
-    RESTORE = 7
+    RESTORE = 7,
+    RESTORE_CONTENT = 8
 };
 
 /**
@@ -34,12 +40,17 @@ enum MESSAGE_TYPE : int {
  * @version 1.0.0
  */
 class Message {
-    MESSAGE_TYPE code;
+    enum {
+        type_length = sizeof(int),
+        length_length = sizeof(size_t)
+    };
+
     Message(MESSAGE_TYPE code = ERROR) : code(code), status(true) {}
 
     friend class boost::serialization::access;
 
 public:
+    MESSAGE_TYPE code;
     std::string username, password;
     std::string path, hash;
     std::vector<std::string> paths;
@@ -48,41 +59,33 @@ public:
     bool status; // = okay
 
     /**
-     * sent this message
-     * @param out_stream
-     * @return stream
+     * send this message
+     * using a TLV format (type, length, value)
+     * @return asio buffer
      */
-    std::ostream send_to(std::ostream out_stream) {
-        boost::archive::text_oarchive oa{ out_stream };
+    std::vector<boost::asio::const_buffer> send() {
+        std::ostringstream type_stream{};
+        std::ostringstream length_stream{};
+        std::ostringstream value_stream{};
+        boost::archive::text_oarchive oa{ value_stream };
 
-        oa << this->code;
-        switch(this->code) {
-            case OKAY:
-                oa << this->status;
-                break;
-            case LOGIN:
-                oa << this->username;
-                oa << this->password;
-                break;
-            case PROBE:
-                oa << this->paths;
-                break;
-            case PROBE_CONTENT:
-                oa << this->hashes;
-                break;
-            case GET:
-                oa << this->path;
-                break;
-            case GET_CONTENT:
-                oa << this->file;
-            case PUSH:
-                oa << this->path;
-                oa << this->file;
-                oa << this->hash;
-                break;
-            default:
-                oa << false;
-        }
+        // serialize message content
+        oa << this;
+        auto value_data = value_stream.str();
+
+        // fixed lengths for the "type" and "length" values
+        type_stream << std::setw(type_length) << std::hex << this->code;
+        length_stream << std::setw(length_length) << std::hex << value_data.length();
+        auto type_data = value_stream.str();
+        auto length_data = value_stream.str();
+
+        // concat buffers into a vector
+        std::vector<boost::asio::const_buffer> out_buffers;
+        out_buffers.push_back(boost::asio::buffer(type_data));
+        out_buffers.push_back(boost::asio::buffer(length_data));
+        out_buffers.push_back(boost::asio::buffer(value_data));
+
+        return out_buffers;
     }
 
     /**
@@ -96,6 +99,7 @@ public:
      * @param in_stream
      */
     Message(std::istream in_stream) : Message() {
+        std::istringstream archive_stream{};
         boost::archive::text_iarchive ia{ in_stream };
 
         MESSAGE_TYPE code;
@@ -133,9 +137,9 @@ public:
      * create a message to say everything is okay
      * @return new message
      */
-    static Message *okay() {
-        auto message = new Message{ OKAY };
-        return message;
+    static std::vector<boost::asio::const_buffer> okay() {
+        Message message{ OKAY };
+        return message.send();
     }
 
     /**
@@ -144,11 +148,11 @@ public:
      * @param password
      * @return new message
      */
-    static Message *login(std::string username = "", std::string password = "") {
-        auto message = new Message{ LOGIN };
-        message->username = username;
-        message->password = password;
-        return message;
+    static std::vector<boost::asio::const_buffer> login(std::string username = "", std::string password = "") {
+        Message message{ LOGIN };
+        message.username = username;
+        message.password = password;
+        return message.send();
     }
 
     /**
@@ -157,10 +161,10 @@ public:
      * @param paths
      * @return new message
      */
-    static Message *probe(std::vector<std::string> paths) {
-        auto message = new Message{ PROBE };
-        message->paths = paths;
-        return message;
+    static std::vector<boost::asio::const_buffer> probe(std::vector<std::string> paths) {
+        Message message{ PROBE };
+        message.paths = paths;
+        return message.send();
     }
 
     /**
@@ -168,10 +172,10 @@ public:
      * @param path
      * @return new message
      */
-    static Message *get(std::string path = "") {
-        auto message = new Message{ GET };
-        message->path = path;
-        return message;
+    static std::vector<boost::asio::const_buffer> get(std::string path = "") {
+        Message message{ GET };
+        message.path = path;
+        return message.send();
     }
 
     /**
@@ -183,12 +187,12 @@ public:
      *
      * DO NOT change the params order, no default value for the std::iostream
      */
-    static Message *push(std::iostream file, std::string path = "", std::string hash = "") {
-        auto message = new Message{ PUSH };
-        message->file = std::vector<unsigned char>{ std::istreambuf_iterator<char>(file), {} };
-        message->path = path;
-        message->hash = hash;
-        return message;
+    static std::vector<boost::asio::const_buffer> push(std::iostream file, std::string path = "", std::string hash = "") {
+        Message message{ PUSH };
+        message.file = std::vector<unsigned char>{ std::istreambuf_iterator<char>(file), {} };
+        message.path = path;
+        message.hash = hash;
+        return message.send();
     }
 
     /**
@@ -196,9 +200,9 @@ public:
      * il returns the list of paths you have to ask to (with the get(...) method)
      * @return new message
      */
-    static Message *restore() {
-        auto message = new Message{ RESTORE };
-        return message;
+    static std::vector<boost::asio::const_buffer> restore() {
+        Message message{ RESTORE };
+        return message.send();
     }
 };
 
