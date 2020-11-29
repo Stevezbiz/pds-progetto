32690ecf77f4247d3e6db36b8e2bb109406c46a3
paologastaldi <s277393@studenti.polito.it>
2020-11-29 15:32:53 +0100

    test: more details and fix small issues

--- test/serialization/test_serialization.cpp
+++ test/serialization/test_serialization.cpp
@@ -63,6 +63,10 @@ int main(int argc, char **argv) {
     auto inner = new Inner{ 42 };
     auto outer = new Outer{ 73, inner };
 
+    std::cout << " --- INIT --- " << std::endl;
+    std::cout << outer->val << std::endl;
+    std::cout << outer->inner->val << std::endl;
+
     // ------------ TEST 1 ------------
 
     std::ostringstream ostream{};
@@ -77,6 +81,7 @@ int main(int argc, char **argv) {
     Outer *new_outer;
     ia >> new_outer;
 
+    std::cout << " --- TEST 1 --- " << std::endl;
     std::cout << new_outer->val << std::endl;
     std::cout << new_outer->inner->val << std::endl;
 
@@ -87,6 +92,8 @@ int main(int argc, char **argv) {
     // other stuffs...
 
     auto new_outer2 = Outer::build(serialized);
+
+    std::cout << " --- TEST 2 --- " << std::endl;
     std::cout << new_outer2->val << std::endl;
     std::cout << new_outer2->inner->val << std::endl;
 
