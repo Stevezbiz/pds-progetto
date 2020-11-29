b40b6b5c2d5c984209c141b3312a6ffdf9db396b
stevezbiz <s270227@studenti.polito.it>
2020-11-29 20:16:50 +0100

    fix: arguments and set_login

--- server/stub_main.cpp
+++ server/stub_main.cpp
@@ -3,9 +3,7 @@
 //
 
 #include <iostream>
-#include <thread>
 #include "../comm/Server_API.h"
-#include "../comm/Socket_API.h"
 
 int main(int argc, char **argv) {
     if (argc != 6) {
@@ -15,15 +13,15 @@ int main(int argc, char **argv) {
 
     boost::asio::io_context ctx;
     boost::asio::ip::tcp::resolver resolver(ctx);
-    auto endpoint_iterator = resolver.resolve({ argv[1], argv[2] });
-    boost::asio::ip::tcp::socket socket{ ctx };
+    auto endpoint_iterator = resolver.resolve({argv[2], argv[3]});
+    boost::asio::ip::tcp::socket socket{ctx};
     boost::asio::connect(socket, endpoint_iterator);
 
-    auto api = new Server_API{ new Socket_API{ std::move(socket) }};
+    auto api = new Server_API{new Socket_API{std::move(socket)}};
 
     Server_API::set_end([]() { return true; });
     Server_API::set_login([](const std::string &username, const std::string &password) {
-        return username.compare("Bellofigo") == 0;
+        return username == "Bellofigo";
     });
     api->run();
 
