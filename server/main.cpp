4e04415c33334002ea4b0f9c6c2f2f6ab0b418d0
stevezbiz <s270227@studenti.polito.it>
2020-12-07 17:03:44 +0100

    feat: Server init

--- server/main.cpp
+++ server/main.cpp
@@ -2,14 +2,14 @@
 #include "Server.h"
 
 int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <port> <root directory> <database directory>" << std::endl;
         exit(-1);
     }
     try {
         boost::asio::io_context ctx;
         boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::tcp::v4(), std::stoi(argv[1]));
-        Server server(ctx, endpoint, "insert database path here", "insert root directory here");
+        Server server(ctx, endpoint, argv[3], argv[2]);
         ctx.run();
     } catch (std::exception &e) {
         std::cerr << e.what() << std::endl;
