aeffc988bdbd6a07c8366fd5cd20328d35be89bf
paologastaldi <s277393@studenti.polito.it>
2020-12-07 10:25:09 +0100

    docs: more logs

--- comm/Client_API.cpp
+++ comm/Client_API.cpp
@@ -32,6 +32,8 @@ bool Client_API::login(const std::string &username, const std::string &password)
 }
 
 bool Client_API::probe(const std::map<std::string, std::string> &map) {
+    Logger::info("Client_API::probe", "Probe check started...", PR_LOW);
+
     std::vector<std::string> paths;
     paths.reserve(map.size());
     for (auto const &item : map)
@@ -50,31 +52,39 @@ bool Client_API::probe(const std::map<std::string, std::string> &map) {
         auto path = item.first;
         auto it = map.find(path);
         if(it == map.end()) { // if the file does not exists, the file has been created
-            if (!this->push(Utils::read_from_file(this->root_path / path), path, map.at(path), ElementStatus::createdFile))
+            Logger::info("Client_API::probe", "Created file found" + path, PR_LOW);
+            if (!this->push(Utils::read_from_file(path), path, map.at(path), ElementStatus::createdFile))
                 return false;
         }
-        else if(map.at(path) != item.second) // if versions (hashes) are different, the file has been modified
-            if(!this->push(Utils::read_from_file(this->root_path / path), path, map.at(path), ElementStatus::modifiedFile))
+        else if(map.at(path) != item.second) { // if versions (hashes) are different, the file has been modified
+            Logger::info("Client_API::probe", "Modified file found " + path, PR_LOW);
+            if (!this->push(Utils::read_from_file(path), path, map.at(path),ElementStatus::modifiedFile))
                 return false;
+        }
     }
 
     for(auto const &item : map) {
         auto path = item.first;
         auto it = map.find(path);
-        if(it == map.end()) // if the file does not exists, the file has been deleted
-            if (!this->push(Utils::read_from_file(this->root_path / path), path, "0x0", ElementStatus::erasedFile))
+        if(it == map.end()) { // if the file does not exists, the file has been deleted
+            Logger::info("Client_API::probe", "Deleted file found " + path, PR_LOW);
+            if (!this->push(Utils::read_from_file(path), path, "0x0", ElementStatus::erasedFile))
                 return false;
+        }
     }
+    Logger::info("Client_API::probe", "Probe check started... - done", PR_LOW);
 
     return true;
 }
 
 bool Client_API::push(const std::vector<unsigned char> &file, const std::string &path, const std::string &hash, ElementStatus elementStatus) {
+    Logger::info("Client_API::push", "Push started...");
     auto req = Message::push(file, path, hash, elementStatus);
     this->api_->send(req);
     if(!this->api_->receive(MSG_OKAY))
         return false;
     auto res = this->api_->get_message();
+    Logger::info("Client_API::push", "Push started... - done");
 
     return res->is_okay();
 }
