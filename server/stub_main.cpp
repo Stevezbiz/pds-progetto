f91a21781947e724f685d57b01fa77cee2024631
paologastaldi <s277393@studenti.polito.it>
2020-12-07 10:24:55 +0100

    fix: update stub server

--- server/stub_main.cpp
+++ server/stub_main.cpp
@@ -16,15 +16,17 @@ int main(int argc, char **argv) {
     boost::asio::ip::tcp::acceptor acceptor{ctx, endpoint};
     boost::asio::ip::tcp::socket socket{ctx};
     acceptor.accept(socket);
-    auto api = new Server_API{new Socket_API{std::move(socket)}};
-    Server_API::set_end([]() { return true; });
-    Server_API::set_login([](const std::string &username, const std::string &password) {
+    // auto api = new Server_API{new Socket_API{std::move(socket)}};
+    auto api = new Server_API{};
+    api->set_end([](const Session &session) { return true; });
+    api->set_login([](const Session &session, const std::string &username, const std::string &password) {
         return username == "Bellofigo";
     });
     std::thread t([&ctx]() {
         ctx.run();
     });
-    api->run();
+    Session session{ std::move(socket) };
+    api->run(session);
     t.join();
     return 0;
 }
