995d5c5d016f548c340b24128ca1b82d363b297f
paologastaldi <s277393@studenti.polito.it>
2020-12-07 09:50:24 +0100

    fix: missing constructor

--- comm/API.cpp
+++ comm/API.cpp
@@ -5,6 +5,7 @@
 #include "API.h"
 
 API::API(Socket_API *socket_api, const std::string &root_path) : api_(socket_api), root_path(root_path) {}
+API::API(const std::string &root_path) : root_path(root_path) {}
 
 bool API::save_file_(Message *message) {
     fs::path full_path = this->root_path / fs::path{ message->path };
