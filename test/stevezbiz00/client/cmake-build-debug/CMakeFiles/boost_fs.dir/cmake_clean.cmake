ab822092b3672accabf3f9d4298bf6e4a92c6e6e
stevezbiz <s270227@studenti.polito.it>
2020-12-10 01:14:03 +0100

    incrociamo i diti

---
+++ test/stevezbiz00/client/cmake-build-debug/CMakeFiles/boost_fs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/boost_fs.dir/FSElement.cpp.o"
+  "CMakeFiles/boost_fs.dir/FileWatcher.cpp.o"
+  "CMakeFiles/boost_fs.dir/main.cpp.o"
+  "boost_fs"
+  "boost_fs.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/boost_fs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
