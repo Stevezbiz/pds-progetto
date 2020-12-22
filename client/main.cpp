0f48f9c628d7777b9da15b4363f8a91c70b66b8e
stevezbiz <s270227@studenti.polito.it>
2020-12-22 12:29:58 +0100

    feat: added logs on file

--- client/main.cpp
+++ client/main.cpp
@@ -10,36 +10,39 @@
 int main(int argc, char **argv) {
     Logger::logger_filter = PR_LOW;
     std::ofstream log_stream;
-//    log_stream.open(LOG_FILE, std::ios_base::app); // append
-    log_stream.open(LOG_FILE, std::ios::out | std::ios::trunc); // erase previous content
-    Logger::redirect(log_stream);
 
     Client *client;
     try {
         ConfigSettings c;
         c.init_configuration();
-        client = new Client{c.getDirPath(), c.getAddress(),c.getPort()};
+        log_stream.open(LOG_FILE, std::ios_base::app); // append
+        log_stream.open(c.get_logs_path(), std::ios::out | std::ios::trunc); // erase previous content
+        Logger::redirect(log_stream);
+        client = new Client{c.getDirPath(), c.getAddress(), c.getPort()};
 
-        if(!client->pwdAttempts()){
+        if (!client->pwdAttempts()) {
             Logger::error("main", "No attempts remaining... terminate the program", PR_HIGH);
+            log_stream.close();
             exit(-1);
         }
 
-        Command cmd = c.menu();
+        Command cmd = ConfigSettings::menu();
         switch (cmd) {
             case normal:
-                if(!client->probe()){
+                if (!client->probe()) {
                     // TODO: error management
                     Logger::error("main", "Probe failed", PR_HIGH);
                     client->close();
+                    log_stream.close();
                     exit(-1);
                 }
                 break;
             case restore:
-                if(!client->restore()) {
+                if (!client->restore()) {
                     // TODO: error management
                     Logger::error("main", "Restore failed", PR_HIGH);
                     client->close();
+                    log_stream.close();
                     exit(-1);
                 }
                 break;
@@ -47,11 +50,12 @@ int main(int argc, char **argv) {
                 return 0;
         }
         client->run();
-        while(true){//If it triggered the third operation -> Quit
-            if (c.question_menu() == 0) {
-                if (c.question_yesno("Are you sure you want to exit?")) {
-                    if(!client->close()){
+        while (true) {//If it triggered the third operation -> Quit
+            if (ConfigSettings::question_menu() == 0) {
+                if (ConfigSettings::question_yesno("Are you sure you want to exit?")) {
+                    if (!client->close()) {
                         Logger::error("main", "client cannot be closed", PR_HIGH);
+                        log_stream.close();
                         exit(-1);
                     }
                     break;
@@ -65,5 +69,6 @@ int main(int argc, char **argv) {
         std::cerr << "Exception: " << e.what() << std::endl;
         client->close();
     }
+    log_stream.close();
     return 0;
 }
